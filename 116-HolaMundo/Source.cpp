5a2f9150e43494c06605b0892590e4a84e6a3652
Javier Gómez <64856260+javigom@users.noreply.github.com>
2021-12-09 11:02:28 +0100

    Update Source.cpp

--- 116-HolaMundo/Source.cpp
+++ 116-HolaMundo/Source.cpp
@@ -1,11 +1,9 @@
 /*
-
-Realizado por Javier Gómez (github.com/javigom) - 10/11/2019
-
-Problema 116 - ¡Hola Mundo! (aceptaelreto.com)
-
-GNU General Public License v3
-
+ * Realizado por Javier Gómez (github.com/javigom) - 10/11/2019
+ *
+ * Problema 116 - ¡Hola Mundo! (aceptaelreto.com)
+ *
+ * GNU General Public License v3
 */
 
 #include <iostream>
